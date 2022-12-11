f3db7ab3952c3e1f5161ddcfbdabd1a5202c10e1
MINGZE HU <mingze.hu@wsu.edu>
2022-12-10 16:53:39 -0800

    update code for testCalculator.jave
    update some test case for milestone3

--- Electrical-Calculator/src/cpts422/calculator/testCalculator.java
+++ Electrical-Calculator/src/cpts422/calculator/testCalculator.java
@@ -119,4 +119,63 @@ class testCalculator {
 		//assertEquals(9.0, expression.Evaluate());
 		assertEquals(0.0, expression.Evaluate());
 	}
+
+	//The new test case for Milestone3(BlackBox)
+	@Test
+	void newTest(){
+		Expression expression1 = new Expression("10+1");
+		assertEquals(11.0, expression1.Evaluate());
+		Expression expression2 = new Expression("22-3");
+		assertEquals(19.0, expression2.Evaluate());
+		Expression expression3 = new Expression("56*2");
+		assertEquals(112, expression3.Evaluate());
+		Expression expression4 = new Expression("99/33");
+		assertEquals(3.0, expression4.Evaluate());
+		Expression expression5 = new Expression("3^3");
+		assertEquals(27.0, expression5.Evaluate());
+		Expression expression6 = new Expression("-2^1024-1");
+		assertEquals("Error", expression6.Evaluate());
+		Expression expression7 = new Expression("2^1024+1");
+		assertEquals("Error", expression7.Evaluate());
+		Expression expression8 = new Expression("2+n");
+		assertEquals("Error", expression8.Evaluate());
+		Expression expression9 = new Expression("2+n");
+		assertEquals("Error", expression9.Evaluate());
+		Expression expression10 = new Expression("2.3/0.7");
+		assertEquals(3.28571428571, expression10.Evaluate());
+		Expression expression11 = new Expression("-4*2");
+		assertEquals("-8.0", expression11.Evaluate());
+		Expression expression12 = new Expression("@");
+		assertEquals("Error", expression12.Evaluate());
+		Expression expression13 = new Expression("29^'");
+		assertEquals("Error", expression13.Evaluate());
+		Expression expression14 = new Expression("3+(5-2)");
+		assertEquals("6.0", expression14.Evaluate());
+		Expression expression15 = new Expression("6*(3+3)");
+		assertEquals("36.0", expression15.Evaluate());
+		Expression expression16 = new Expression("2+2^2");
+		assertEquals("6.0", expression16.Evaluate());
+		Expression expression17 = new Expression("2^(2+2)");
+		assertEquals("16.0", expression17.Evaluate());
+		Expression expression18 = new Expression("2/(2-2)");
+		assertEquals("Error", expression18.Evaluate());
+		Expression expression19 = new Expression("2^1024-2^1024");
+		assertEquals("Error", expression19.Evaluate());
+		Expression expression20 = new Expression("2+)(3");
+		assertEquals("Error", expression20.Evaluate());
+		Expression expression21 = new Expression("(0)");
+		assertEquals("0.0", expression21.Evaluate());
+		Expression expression22 = new Expression("(1)+(34)");
+		assertEquals("35.0", expression22.Evaluate());
+		Expression expression23 = new Expression("22/(0)");
+		assertEquals("Error", expression23.Evaluate());
+		Expression expression24 = new Expression("22^(0)");
+		assertEquals("1", expression24.Evaluate());
+		Expression expression25 = new Expression("2^(-1)");
+		assertEquals("0.5", expression25.Evaluate());
+		Expression expression26 = new Expression("one+two+three");
+		assertEquals("Error", expression26.Evaluate());
+		Expression expression27 = new Expression("-4^2");
+		assertEquals("16.0", expression27.Evaluate());
+	}
 }
